0f7b2b608c57268d6d5912521f1b5448943a1a32
Sarkanyolo <gyuri.deak@gmail.com>
2018-12-14 14:07:47 +0100

    Improve 2018 Day 13

--- 2018/Day13/Day13.js
+++ 2018/Day13/Day13.js
@@ -24,6 +24,40 @@ class Car {
     }
   }
 
+  static compareCars(x, y) {
+    const res = x.y - y.y;
+    return res !== 0 ? res : x.x - y.x;
+  }
+
+  getNextCoord() {
+    const coord = Object;
+    coord.x = this.x;
+    coord.y = this.y;
+
+    switch (this.direction) {
+      case 'up':
+        coord.y -= 1;
+        break;
+      case 'down':
+        coord.y += 1;
+        break;
+      case 'left':
+        coord.x -= 1;
+        break;
+      case 'right':
+        coord.y += 1;
+        break;
+      default:
+        throw new Error('Invalid direction');
+    }
+
+    return coord;
+  }
+
+  go(char) {
+    // TODO
+  }
+
   turnLeft() {
     switch (this.direction) {
       case 'left':
@@ -50,34 +84,66 @@ class Car {
   }
 }
 
-function getMap(x, y) {
-  const map = new Array(y);
-  for (let i = 0; i < y; i += 1) {
-    map[i] = new Array(x).fill('#');
+class Map {
+  constructor(x, y) {
+    this.map = new Array(y);
+    for (let i = 0; i < y; i += 1) {
+      this.map[i] = new Array(x);
+    }
   }
 
-  return map;
-}
+  static toValidChar(c) {
+    if (c === 'v' || c === '^') return '|';
+    if (c === '<' || c === '>') return '-';
+    return c.trim();
+  }
 
-function getMapChar(c) {
-  if (c === 'v' || c === '^') return '|';
-  if (c === '<' || c === '>') return '-';
-  return c;
+  setCoord(x, y, char) {
+    this.map[y][x] = Map.toValidChar(char);
+  }
+
+  getCoord(x, y) {
+    return this.map[y][x];
+  }
 }
 
 const cars = [];
 const signs = 'v^<>';
-const map = getMap(input[0].length, input.length);
+const map = new Map(input[0].length, input.length);
 
 for (let y = 0; y < input.length; y += 1) {
   for (let x = 0; x < input[y].length; x += 1) {
     const char = input[y][x];
-    map[y][x] = getMapChar(char);
+    map.setCoord(x, y, char);
     if (signs.includes(char)) {
-      cars.push(new Car(x, y, char));
+      cars.push(new Car(x, y, char, map));
     }
   }
 }
 
-console.dir(cars);
-console.dir(map);
+function hasCrash() {
+  for (let i = 0; i < cars.length - 1; i += 1) {
+    for (let j = i + 1; j < cars.length; j += 1) {
+      if (cars[i].x === cars[j].x && cars[i].y === cars[j].y) {
+        console.log(cars[i].x, cars[i].y);
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+function run() {
+  while (true) {
+    cars.sort(Car.compareCars);
+    for (let i = 0; i < cars.length; i += 1) {
+      const coord = cars[i].getNextCoord();
+      const char = map.getCoord(coord.x, coord.y);
+      cars[i].go(char);
+      if (hasCrash()) return 0;
+    }
+  }
+}
+
+run();
